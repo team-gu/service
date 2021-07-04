a96e568d4928db90c7dcef15417f99b9a66789b8
YONGJAE LEE <dev.yongjaelee@gmail.com>
2021-07-04 23:05:59 +0900

    chore(alias): module alias 설정을 위해 babel, next.config, tsconfig 설정한다
    https://vallista.kr/2019/05/15/Next%EC%97%90%EC%84%9C-Alias-%EC%A0%81%EC%9A%A9%ED%95%98%EA%B8%B0

--- next.config.js
+++ next.config.js
@@ -1,3 +1,33 @@
+const path = require('path');
+
 module.exports = {
   reactStrictMode: true,
-}
+  webpack(config, options) {
+    config.resolve = {
+      alias: {
+        '@atoms': path.resolve(__dirname, 'components/atoms'),
+        '@molecules': path.resolve(__dirname, 'components/molecules'),
+        '@organisms': path.resolve(__dirname, 'components/organisms'),
+        '@templates': path.resolve(__dirname, 'pages/template'),
+        '@pages': path.resolve(__dirname, 'pages'),
+        '@assets': path.resolve(__dirname, 'assets'),
+        '@hooks': path.resolve(__dirname, 'hooks'),
+        '@mocks': path.resolve(__dirname, 'mocks'),
+        '@repository': path.resolve(__dirname, 'repository'),
+        '@routes': path.resolve(__dirname, 'routes'),
+        '@store': path.resolve(__dirname, 'store'),
+        '@style': path.resolve(__dirname, 'style'),
+        '@utils': path.resolve(__dirname, 'utils'),
+        '@context': path.resolve(__dirname, 'context'),
+        '@parse': path.resolve(__dirname, 'test'),
+      },
+      ...config.resolve,
+    };
+
+    // config.resolve.alias['@src'] = path.join(__dirname, 'src')
+    // config.resolve.alias['@components'] = path.join(__dirname, 'src', 'components')
+    // config.resolve.alias['@interface'] = path.join(__dirname, 'src', '@types/interface.ts')
+    // config.resolve.extensions = ['js', 'ts', 'tsx']
+    return config;
+  },
+};
