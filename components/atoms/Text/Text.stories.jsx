6787099d2315c9fadb2e43e21fe1e6dbef0e82b9
YONGJAE LEE <dev.yongjaelee@gmail.com>
2021-07-12 19:18:49 +0900

    feat: Image, Text 컴포넌트의 스토리를 추가한다
    * 간단하게 추가

---
+++ components/atoms/Text/Text.stories.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import Text from './Text';
+
+export default {
+  title: 'Atoms/Text',
+  component: Text,
+};
+
+export const text = () => <Text text="abc" />;
