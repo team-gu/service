1646fccbb04c9c9b92d9ce17e548c496233a73d9
YONGJAE LEE <dev.yongjaelee@gmail.com>
2021-07-14 22:40:12 +0900

    refactor: _app에 공통된 컴포넌트를 추가한다
    * global style, navbar 등

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,11 @@ import { ThemeProvider } from 'styled-components';
 
 import type { AppProps } from 'next/app';
 
+import { Modal, Navbar } from '@organisms';
+import { Spinner } from '@molecules';
+import { MODALS } from '@utils/constants';
+import GlobalStyle from '@styles/globalStyles';
+
 import store from '@store';
 import theme from '@styles/theme';
 
@@ -10,7 +15,13 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
+        <GlobalStyle />
+        <Navbar />
+        <section style={{ marginTop: '83px' }}>
+          <Component {...pageProps} />
+        </section>
+        <Modal modalName={MODALS.ALERT_MODAL} />
+        <Spinner />
       </ThemeProvider>
     </Provider>
   );
