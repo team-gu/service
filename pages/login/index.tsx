216f15fa37b9f367e30d2a9d47327190e885b524
이용재 <201704147@cs-cnu.org>
2021-07-16 23:56:04 +0900

    fix: pages에 해당하는 컴포넌트에 사용하지 않는 코드를 제거한다
    * 검증용 코드 제거

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,27 +1,8 @@
-import { ReactElement, useEffect } from 'react';
+import { ReactElement } from 'react';
 
-import styled from 'styled-components';
-import { useAppSelector } from '@hooks';
 import { LoginComponent } from '@organisms';
 
-import { get } from '@utils/snippet';
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({
-    theme: {
-      colors: { black },
-    },
-  }) => black};
-`;
-
 export default function Login(): ReactElement {
-  const { content } = useAppSelector(get('modal'));
-
-  useEffect(() => {
-    console.log(content);
-  }, [content]);
-
   return (
     <>
       <LoginComponent />
--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -1,7 +1,4 @@
-import { ReactElement, useRef } from 'react';
-import { useRouter } from 'next/router';
-
-import styled from 'styled-components';
+import { ReactElement } from 'react';
 import { RegisterComponent } from '@organisms';
 
 export default function User(): ReactElement {
