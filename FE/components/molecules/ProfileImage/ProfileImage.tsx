b7c5cfd723745701ec27df98813a04e87cf8700c
이용재 <201704147@cs-cnu.org>
2021-07-25 23:17:00 +0900

    feat: ProfileImage에서 active 상태임을 나타내는 띠를 구현한다
    * 채팅 작업시 필요한 부분
    https://jira.ssafy.com/browse/S05P12A202-300
    [#S05P12A202-300]

--- FE/components/molecules/ProfileImage/ProfileImage.tsx
+++ FE/components/molecules/ProfileImage/ProfileImage.tsx
@@ -5,20 +5,28 @@ import styled from 'styled-components';
 interface ProfileImageProps {
   src: string;
   size?: number;
+  isActive?: boolean;
 }
 
-const Wrapper = styled.div<{ size: number }>`
+const Wrapper = styled.div<{ size: number; isActive: boolean }>`
   border-radius: ${({ size }) => size}px;
   width: ${({ size }) => size}px;
   height: ${({ size }) => size}px;
+  ${({
+    isActive,
+    theme: {
+      colors: { daangn },
+    },
+  }) => isActive && `border: 3px solid ${daangn};`}
 `;
 
 export default function ProfileImage({
   src = '/profile.svg',
   size = 50,
+  isActive = false,
 }: ProfileImageProps): ReactElement {
   return (
-    <Wrapper size={size}>
+    <Wrapper size={size} isActive={isActive}>
       <Image src={src} alt="profile" width={size} height={size} />
     </Wrapper>
   );
