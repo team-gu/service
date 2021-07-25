9b69ac7a042e25d4d99fef4a17139df03ddb7e70
이용재 <201704147@cs-cnu.org>
2021-07-25 21:05:01 +0900

    feat: Checkbox HOC 컴포넌트를 제작하고 스토리를 작성한다
    * 최소한의 기능이 동작하도록 처리했음
    https://jira.ssafy.com/browse/S05P12A202-116
    [#S05P12A202-116]

--- FE/components/molecules/index.ts
+++ FE/components/molecules/index.ts
@@ -1,6 +1,7 @@
 import Button from './Button';
+import Checkbox from './Checkbox';
 import FloatButton from './FloatButton';
 import Label from './Label';
 import Spinner from './Spinner';
 
-export { Button, FloatButton, Label, Spinner };
+export { Button, Checkbox, FloatButton, Label, Spinner };
