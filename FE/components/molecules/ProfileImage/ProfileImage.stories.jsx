668d35c9cbc2db4838f185c3018ed464f905aadf
이용재 <201704147@cs-cnu.org>
2021-07-25 23:20:35 +0900

    refactor: ProfileImage 스토리를 ts로 변경하고 arg bind를 사용하도록 한다
    * 이렇게 설정해야 controls에서 조작할 수 있음
    https://jira.ssafy.com/browse/S05P12A202-302
    [#S05P12A202-302]

--- FE/components/molecules/ProfileImage/ProfileImage.stories.jsx
+++
@@ -1,8 +0,0 @@
-import ProfileImage from './ProfileImage';
-
-export default {
-  title: 'Molecules/ProfileImage',
-  component: ProfileImage,
-};
-
-export const profileImage = () => <ProfileImage src={'/profile.svg'} />;
