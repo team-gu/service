2fa73d8ad0bcd9e94016eff79f568d648fac5d1a
CODe1995 <code.57x53@gmail.com>
2021-07-27 02:01:02 +0900

    fix: build test단계 안되던 문제 수정
    - test 주석 처리 -> 보완 필요

---
+++ BE/teamgu/src/main/java/com/teamgu/config/GlobalPropertySource.java
@@ -0,0 +1,40 @@
+package com.teamgu.config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+ 
+@Configuration
+@PropertySources({
+    @PropertySource( value = "file:c:/rds_secure/config.yml", ignoreResourceNotFound = true ),
+    @PropertySource( value = "file:${user.home}/rds_secure/config.yml", ignoreResourceNotFound = true)
+})
+public class GlobalPropertySource { 
+    @Value("${spring.datasource.driverClassName}")
+    private String driverClassName;
+    
+    @Value("${spring.datasource.url}")
+    private String url;
+    
+    @Value("${spring.datasource.username}")
+    private String username;
+    
+    @Value("${spring.datasource.password}")
+    private String password;
+ 
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+ 
+    public String getUrl() {
+        return url;
+    }
+ 
+    public String getUsername() {
+        return username;
+    }
+ 
+    public String getPassword() {
+        return password;
+    } 
+}
