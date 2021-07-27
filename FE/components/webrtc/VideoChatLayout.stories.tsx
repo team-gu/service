00554c2acae3091ddbb69e70227872c0e759892c
minho-jang <minho9301@naver.com>
2021-07-27 17:08:43 +0900

    feat: 화상채팅 레이아웃 스토리북

--- FE/components/webrtc/VideoChatLayout.stories.tsx
+++ FE/components/webrtc/VideoChatLayout.stories.tsx
@@ -5,41 +5,64 @@ export default {
   title: 'webrtc/Video Chat Layout',
 };
 
-const arr = [1,2,3,4,5,6,7,8,9];
+const arr = [1,2,2,2];
 
 // 9 ~
 const Wrapper = styled.div<{ number: number }>`
   display: flex;
   flex-flow: row wrap;
   justify-content: center;
-  flex-grow: 1;
+  align-items: center;
 
   .item {
     background-color: yellow;
     border: solid red 1px;
   }
 
-  ${({number}) => {
-    if (number > 8) {
-      return `.item { flex: 0 0 240px }`
+  .item > .video {
+    margin: 0 auto;
+    background-color: skyblue;
+    width: 640px;
+    height: 320px;
+  }
 
-    } else if (number > 4) {
-      return `.item { flex: 0 0 360px }`
+  .item > .profile {
+    margin: 0 auto;
+    background-color: gray;
+    width: 240px;
+    height: 240px;
+  }
 
-    } else if (number > 1) {
-      return `.item { flex: 0 0 480px }`
-
-    } else {
-      return `.item { flex: 0 0 640px }`
-    }
-  }}
+  .item {
+    flex: 0 0 640px;
+  }
 `;
+
+// ${({ number }) => {
+//     if (number > 8) {
+//       return `.item { flex: 0 0 240px }`;
+//     } else if (number > 4) {
+//       return `.item { flex: 0 0 360px }`;
+//     } else if (number > 2) {
+//       return `.item { flex: 0 0 480px }`;
+//     } else {
+//       return `.item { flex: 0 0 640px }`;
+//     }
+//   }}
 
 const Template: Story = () => (
   <Wrapper number={arr.length}>
-    {arr.map((item, index) => (
-      <div key={index} className="item">{item}</div>
-    ))}
+    {arr.map((item, index) =>
+      item % 2 == 0 ? (
+        <div key={index} className="item">
+          <div className="profile">{item}</div>
+        </div>
+      ) : (
+        <div key={index} className="item">
+          <div className="video">{item}</div>
+        </div>
+      ),
+    )}
   </Wrapper>
 );
 
