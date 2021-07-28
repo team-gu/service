cde2a896d18ca77888c45352e1f98096928f212d
seonmee <gus_tjs_al@naver.com>
2021-07-29 01:11:43 +0900

    feat: 복합키 추가 & 공통코드 매핑 repository를 추가

--- BE/teamgu/src/main/java/com/teamgu/database/repository/AwardRepository.java
+++ BE/teamgu/src/main/java/com/teamgu/database/repository/AwardRepository.java
@@ -1,5 +1,7 @@
 package com.teamgu.database.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.teamgu.database.entity.UserAward;
 
 @Repository
 public interface AwardRepository extends JpaRepository<UserAward, Long> {
+
+	Optional<UserAward> findByName(String name);
 
 }
