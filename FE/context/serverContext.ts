7c74dbf517a44fe13158806538e7c3479de885eb
Dongkyun_Jang <thd6576@naver.com>
2021-07-30 02:07:07 +0900

    fix: CORS 에러 해결을 위한 헤더 설정 추가

--- FE/context/serverContext.ts
+++ FE/context/serverContext.ts
@@ -7,14 +7,22 @@ interface ApiParameter {
   param?: any;
 }
 
-export const SERVER_URL: string =
-  process.env.REACT_APP_API_SERVER_URL || 'http://localhost:8000';
+export const SERVER_URL: string = 'http://i5a202.p.ssafy.io:8080';
 
 const api = ({ url, type = 'get', param }: ApiParameter) => {
   const accessToken = loadItem('accessToken');
-
-  const headers: { 'Content-Type': string; Authorization?: string } = {
+  const headers: {
+    'Content-Type': string;
+    Authorization?: string;
+    'Access-Control-Allow-Origin': string;
+    'Access-Control-Allow-Methods': string;
+    'Access-Control-Allow-Headers': string;
+  } = {
     'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    'Access-Control-Allow-Headers':
+      'Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization',
   };
 
   if (accessToken !== null) {
