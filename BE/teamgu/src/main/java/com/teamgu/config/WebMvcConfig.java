fff8f5de58371276bb86303b79e078221e88c2a4
CODe1995 <code.57x53@gmail.com>
2021-07-29 14:21:42 +0900

    feat: WebMvcConfigurer를 통한 CORS 설정

---
+++ BE/teamgu/src/main/java/com/teamgu/config/WebMvcConfig.java
@@ -0,0 +1,39 @@
+package com.teamgu.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import io.netty.handler.codec.http.HttpMethod;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        // configuration.addAllowedOrigin("*");
+//        configuration.addAllowedOriginPattern("*");
+//        configuration.addAllowedMethod("*");
+//        configuration.addAllowedHeader("*");
+//        configuration.addExposedHeader(JwtTokenUtil.HEADER_STRING);
+//        configuration.setAllowCredentials(true);
+//        configuration.setMaxAge(3600L);
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+	    return new WebMvcConfigurer() {
+	        @Override
+	        public void addCorsMappings(CorsRegistry registry) {
+	            registry.addMapping("/**")
+	                    .allowedOrigins("*")
+	                    .allowedMethods(HttpMethod.POST.name())
+	                    .allowCredentials(false)
+	                    .maxAge(3600);
+	        }
+	    };
+	}
+}
