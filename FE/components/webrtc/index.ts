76fa0f8fdf546d517bc4ab929dba9a78f92cd8a7
minho-jang <minho9301@naver.com>
2021-07-29 10:45:43 +0900

    fix: prettier lint

--- FE/components/webrtc/index.ts
+++ FE/components/webrtc/index.ts
@@ -1,7 +1,17 @@
-import VideoChat from "./VideoChat";
-import VideoRoomConfigModal from "./VideoRoomConfigModal";
-import UserVideoComponent from "./UserVideoComponent";
-import OpenViduVideoComponent from "./OpenViduVideoComponent";
-import VideoChatToolbar from "./VideoChatToolbar";
+import VideoChat from './VideoChat';
+import VideoRoomConfigModal from './VideoRoomConfigModal';
+import UserVideoComponent from './UserVideoComponent';
+import OpenViduVideoComponent from './OpenViduVideoComponent';
+import VideoChatToolbar from './VideoChatToolbar';
+import FloatingCounter from './FloatingCounter';
+import FloatingChatButton from './FloatingChatButton';
 
-export { VideoChat, VideoRoomConfigModal, UserVideoComponent, OpenViduVideoComponent, VideoChatToolbar };
+export {
+  VideoChat,
+  VideoRoomConfigModal,
+  UserVideoComponent,
+  OpenViduVideoComponent,
+  VideoChatToolbar,
+  FloatingCounter,
+  FloatingChatButton,
+};
