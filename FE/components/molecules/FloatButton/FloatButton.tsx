0722829b4402854969e0503f6b5207dc181009a1
이용재 <201704147@cs-cnu.org>
2021-07-29 20:18:18 +0900

    refactor: FloatingChatButton 과 FloatButton의 중복을 제거한다
    * FloatingChatButton에서 FloatButton을 사용하도록 변경

--- FE/components/molecules/FloatButton/FloatButton.tsx
+++ FE/components/molecules/FloatButton/FloatButton.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from 'react';
+import { ReactElement, MouseEventHandler } from 'react';
 import { motion } from 'framer-motion';
 
 import styled from 'styled-components';
@@ -6,43 +6,57 @@ import styled from 'styled-components';
 import { Icon } from '@atoms';
 
 interface FloatButtonProps {
-  func: Function;
+  func?: MouseEventHandler<HTMLSpanElement>;
+  isRtc?: boolean;
 }
 
-const Wrapper = styled(motion.div)`
+const Wrapper = styled(motion.div)<{ isRtc: boolean }>`
   ${({ theme: { flexRow } }) => flexRow()}
 
   position: fixed;
-  bottom: 20px;
-  right: 20px;
-
-  width: 80px;
-  height: 80px;
-  border-radius: 80px;
-
-  box-shadow: 0 6px 12px 0 rgba(4, 4, 161, 0.1);
-
   &,
   i {
     cursor: pointer;
   }
+  ${({ isRtc }) =>
+    isRtc
+      ? `
+    bottom: 50px;
+    right: 30px;
 
-  :hover {
-    opacity: 0.8;
-  }
+    width: 50px;
+    height: 50px;
+    border-radius: 60px;
+
+    opacity: 1;
+
+    :hover {
+      opacity: 0.75;
+    }
+  `
+      : `
+    bottom: 20px;
+    right: 20px;
+
+    width: 80px;
+    height: 80px;
+    border-radius: 80px;
+
+    box-shadow: 0 6px 12px 0 rgba(4, 4, 161, 0.1);
+
+    :hover {
+      opacity: 0.8;
+    }
+  `}
 `;
 
-export default function FloatButton({ func }: FloatButtonProps): ReactElement {
+export default function FloatButton({
+  func = () => {},
+  isRtc = false,
+}: FloatButtonProps): ReactElement {
   return (
-    <Wrapper
-      onClick={() => func()}
-      whileTap={{ rotate: 90, scale: 0.9 }}
-      whileHover={{
-        scale: 1.05,
-        transition: { duration: 1 },
-      }}
-    >
-      <Icon iconName="message" size="25px" />
+    <Wrapper onClick={func} whileTap={{ rotate: 90, scale: 0.9 }} isRtc={isRtc}>
+      <Icon iconName="message" size="24px" />
     </Wrapper>
   );
 }
--- FE/components/webrtc/FloatingChatButton.tsx
+++ FE/components/webrtc/FloatingChatButton.tsx
@@ -1,9 +1,10 @@
 import { ReactElement, MouseEventHandler } from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
+import { FloatButton } from '@molecules';
 import { Icon } from '@atoms';
 
-interface FloatChatBtnProps {
+interface FloatChatButtonProps {
   handleClick: MouseEventHandler<HTMLSpanElement>;
 }
 
@@ -24,36 +25,13 @@ const ChatBtnBackground = styled.div`
   cursor: pointer;
 `;
 
-const ChatBtnContainer = styled(motion.div)`
-  ${({ theme: { flexRow } }) => flexRow()}
-
-  position: fixed;
-  bottom: 50px;
-  right: 30px;
-
-  width: 50px;
-  height: 50px;
-  border-radius: 60px;
-
-  opacity: 1;
-  :hover {
-    opacity: 0.75;
-  }
-
-  i {
-    cursor: pointer;
-  }
-`;
-
 export default function FloatingChatButton({
   handleClick,
-}: FloatChatBtnProps): ReactElement {
+}: FloatChatButtonProps): ReactElement {
   return (
     <span onClick={handleClick}>
       <ChatBtnBackground />
-      <ChatBtnContainer whileTap={{ rotate: 90, scale: 0.9 }}>
-        <Icon iconName="message" />
-      </ChatBtnContainer>
+      <FloatButton isRtc />
     </span>
   );
 }
