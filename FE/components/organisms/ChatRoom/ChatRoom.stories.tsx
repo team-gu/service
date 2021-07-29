6a30237fef945f679cc9ce7b1fe316174316ef22
이용재 <201704147@cs-cnu.org>
2021-07-30 01:35:54 +0900

    fix: ChatRoom 컴포넌트 스타일 정리 및 상수를 사용하도록 변경한다
    * 최소한의 변화

--- FE/components/organisms/ChatRoom/ChatRoom.stories.tsx
+++ FE/components/organisms/ChatRoom/ChatRoom.stories.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Story, Meta } from '@storybook/react';
 import ChatRoom from './ChatRoom';
+import { CHAT_DUMMY_DATA } from '@utils/constants';
 
 export default {
   title: 'Molecules/Chat Room',
@@ -26,71 +27,5 @@ const Template: Story = ({ data }) => {
 export const chatRoom = Template.bind({});
 
 chatRoom.args = {
-  data: [
-    {
-      id: '0',
-      userName: '김싸피',
-      profileSrc: '/profile.png',
-      time: '2021-07-28T17:41:27.699+09:00',
-      message: 'asdfsd',
-      isMe: false,
-    },
-    {
-      id: '1',
-      userName: 'Me',
-      profileSrc: '/profile.png',
-      time: '2021-07-28T17:41:27.699+09:00',
-      message: 'asdfsd',
-      isMe: true,
-    },
-    {
-      id: '2',
-      userName: '김싸피',
-      profileSrc: '/profile.png',
-      time: '2021-07-28T17:41:27.699+09:00',
-      message: 'asdfsd',
-      isMe: false,
-    },
-    {
-      id: '3',
-      userName: '김싸피',
-      profileSrc: '/profile.png',
-      time: '2021-07-28T17:41:27.699+09:00',
-      message: 'asdfsd',
-      isMe: false,
-    },
-    {
-      id: '4',
-      userName: '김싸피',
-      profileSrc: '/profile.png',
-      time: '2021-07-28T17:41:27.699+09:00',
-      message: 'asdfsd',
-      isMe: false,
-    },
-    {
-      id: '5',
-      userName: 'Me',
-      profileSrc: '/profile.png',
-      time: '2021-07-28T17:41:27.699+09:00',
-      message: 'asdfsd',
-      isMe: true,
-    },
-    {
-      id: '6',
-      userName: '김싸피',
-      profileSrc: '/profile.png',
-      time: '2021-07-28T17:41:27.699+09:00',
-      message: 'asdfsd',
-      isMe: false,
-    },
-    {
-      id: '7',
-      userName: '김싸피',
-      profileSrc: '/profile.png',
-      time: '2021-07-28T17:41:27.699+09:00',
-      message:
-        'asdfsdasdfsdasdfsdasdfsdasdfsdasdfsdasdfsdasdfsdasdfsdasdfsdasdfsdasdfsdasdfsdasdfsdasdfsdasdfsd',
-      isMe: false,
-    },
-  ],
+  data: CHAT_DUMMY_DATA,
 };
