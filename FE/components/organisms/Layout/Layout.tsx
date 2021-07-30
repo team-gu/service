438512cdbecdcfa694520957b29e9316e0685f85
minho-jang <minho9301@naver.com>
2021-07-30 17:29:17 +0900

    feat: 레이아웃 변경
    화상채팅 화면에서는 네브바, 푸터를 제외한 컨텐츠 및 채팅만 보이도록 한다.

--- FE/components/organisms/Layout/Layout.tsx
+++ FE/components/organisms/Layout/Layout.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/router';
 import { useUiState, useAppDispatch, setChatOpen } from '@store';
 import { ChatRoute } from '@organisms';
 import { FloatingButton } from '@molecules';
+import { VIDEO_CHAT_PATH_PREFIX } from '@utils/constants';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -22,27 +23,35 @@ export default function Layout({ children }: LayoutProps) {
 
   return (
     <>
-      <Navbar />
-      <Wrapper
-        key={router.route}
-        initial="pageInitial"
-        animate="pageAnimate"
-        variants={{
-          pageInitial: {
-            opacity: 0,
-          },
-          pageAnimate: {
-            opacity: 1,
-          },
-        }}
-      >
-        {children}
-        {isChatOpen && <ChatRoute />}
-        <FloatingButton
-          func={() => dispatch(setChatOpen({ isChatOpen: true }))}
-        />
-      </Wrapper>
-      <Footer />
+      {router.pathname.startsWith(VIDEO_CHAT_PATH_PREFIX) ? (
+        <>
+          {children}
+        </>
+      ) : (
+        <>
+          <Navbar />
+          <Wrapper
+            key={router.route}
+            initial="pageInitial"
+            animate="pageAnimate"
+            variants={{
+              pageInitial: {
+                opacity: 0,
+              },
+              pageAnimate: {
+                opacity: 1,
+              },
+            }}
+          >
+            {children}
+            {isChatOpen && <ChatRoute />}
+            <FloatingButton
+              func={() => dispatch(setChatOpen({ isChatOpen: true }))}
+            />
+          </Wrapper>
+          <Footer />
+        </>
+      )}
     </>
   );
 }
