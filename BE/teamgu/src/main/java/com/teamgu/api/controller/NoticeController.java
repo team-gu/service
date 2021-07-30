e342a4e4dff991c0871574afccea686b97809ea0
donggillee-dev <donggillee94@gmail.com>
2021-07-31 03:08:35 +0900

    comment : NoticeController 주석 수정 및 추가

--- BE/teamgu/src/main/java/com/teamgu/api/controller/NoticeController.java
+++ BE/teamgu/src/main/java/com/teamgu/api/controller/NoticeController.java
@@ -22,16 +22,26 @@ public class NoticeController {
     @Autowired
     NoticeServiceImpl noticeService;
 
-    @GetMapping //공지사항 목록 가져오기
+    /**
+     * 공지사항 목록 Api
+     *
+     * @param pageable
+     * @return ResponseEntity
+     */
+    @GetMapping
     @ApiOperation(value = "공지사항 목록 가져오기", notes = "공지사항 목록에 대한 pagination 정보를 반환")
-    public ResponseEntity<? extends BasicResponse> getNoticeList(
-            Pageable pageable
-    ) {
-
+    public ResponseEntity<? extends BasicResponse> getNoticeList(Pageable pageable) {
         return ResponseEntity.ok(new CommonResponse<Page<NoticeListResDto>>(noticeService.getNoticeList(pageable)));
     }
 
-    @GetMapping("/{id}") //특정 공지사항의 id를 기반으로 세부정보 조회, 공지사항 목록에서 선택하게끔 되는 로직이기에 null을 리턴할 수 없다 => 즉 ok status만 리턴
+    /**
+     * 공지사항 세부정보 Api
+     * 공지사항 목록에서 선택하게끔 되는 로직이기에 not Nullable => ok status만 리턴
+     *
+     * @param id
+     * @return ResponseEntity
+     */
+    @GetMapping("/{id}")
     @ApiOperation(value = "공지사항 세부내용 가져오기", notes = "선택한 공지사항에 대해 세부 정보를 반환")
     public ResponseEntity<? extends BasicResponse> getNoticeDetail(
             @PathVariable @ApiParam(value = "공지사항의 id", required = true) long id
@@ -47,7 +57,15 @@ public class NoticeController {
         return ResponseEntity.ok(new CommonResponse<NoticeDetailResDto>(oNoticeDetail));
     }
 
-    @DeleteMapping("/{id}") //특정 공지사항 id 기반으로 삭제
+    /**
+     * 공지사항 삭제 Api
+     * 존재하지 않을 경우 404오류와 문구 리턴
+     * 제대로 삭제된 경우 빈 Content를 리턴한다
+     *
+     * @param id
+     * @return ResponseEntity
+     */
+    @DeleteMapping("/{id}")
     @ApiOperation(value = "공지사항 삭제하기", notes = "특정 공지사항 지우고 성공 여부 리턴")
     public ResponseEntity<? extends BasicResponse> deleteNotice(
             @PathVariable @ApiParam(value = "공지사항의 id", required = true) long id
@@ -58,10 +76,18 @@ public class NoticeController {
                     .body(new ErrorResponse("일치하는 공지사항이 없습니다."));
         }
 
-        return ResponseEntity.noContent().build(); //제대로 삭제되었으면 그냥 빈 객체 리턴
+        return ResponseEntity.noContent().build();
     }
 
-    @PostMapping //공지사항 등록
+    /**
+     * 공지사항 등록 Api
+     * 생성 오류시 500오류와 문구 리턴
+     * 성공일 경우 빈 Content 리턴
+     *
+     * @param noticeReqDto
+     * @return ResponseEntity
+     */
+    @PostMapping
     @ApiOperation(value = "공지사항 등록하기", notes = "NoticeReqDto 형태로 입력이 들어와 성공 여부 리턴")
     public ResponseEntity<? extends BasicResponse> createNotice(
             @ApiParam(value = "공지사항 VO", required = true) NoticeReqDto noticeReqDto
@@ -75,13 +101,22 @@ public class NoticeController {
         return ResponseEntity.noContent().build();
     }
 
+    /**
+     * 공지사항 수정 Api
+     * 수정 오류시 500오류와 문구 리턴
+     * 성공일 경우 빈 Content 리턴
+     *
+     * @param id
+     * @param noticeReqDto
+     * @return ResponseEntity
+     */
     @PatchMapping("/{id}")
     @ApiOperation(value = "공지사항 수정하기", notes = "수정할 공지사항의 id, NoticeReqDto 형태로 입력이 들어와 성공 여부 리턴")
     public ResponseEntity<? extends BasicResponse> updateNotice(
             @PathVariable @ApiParam(value = "공지사항 id", required = true) long id,
             @ApiParam(value = "공지사항 VO", required = true) NoticeReqDto noticeReqDto
     ) {
-        if(!noticeService.updateNotice(id, noticeReqDto)) {
+        if (!noticeService.updateNotice(id, noticeReqDto)) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(new ErrorResponse("공지사항 수정에 실패하였습니다."));
         }
