955764ec5f74ef26381d47c3d75b081b9d6f40f4
Dongkyun_Jang <thd6576@naver.com>
2021-08-04 03:20:31 +0900

    chore: 필요없는 console.log를 지웁니다

--- FE/store/authSlice.ts
+++ FE/store/authSlice.ts
@@ -63,7 +63,6 @@ export const setLogin =
       // TODO: 리프레시 토큰 사용이 후순위로 밀려서 후에 이 부분에 대한 수정과 사용이 필요합니다.
       // saveItem('refreshToken', data.refreshToken);
       dispatch(setUser(data.userInfo));
-      console.log(data.userInfo);
       data.userInfo.introduce === ''
         ? router.push('/userdetail')
         : router.push('/humanpool');
