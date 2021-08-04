9656204113405bfc047dcdf0ca4adfe6f60b8c3f
장민호 <minho9301@naver.com>
2021-08-05 00:14:42 +0900

    fix: 타입 수정
    타입 정의하면서 놓쳤던 부분 수정

--- FE/repository/baseRepository.ts
+++ FE/repository/baseRepository.ts
@@ -1,5 +1,5 @@
 import api from '@context/serverContext';
-import { Skill, Team, Member } from '@utils/type';
+import { Skill, Team, Member, MemberOption } from '@utils/type';
 
 export const postLoginApi = async (param: object) =>
   await api({
@@ -28,39 +28,47 @@ export const getCheckIdDuplicate = async (param: string) =>
   });
 
 export const getUserListByNameContains = async (param: string) => {
-  return new Promise<Member[]>((resolve) => {
+  return new Promise<MemberOption[]>((resolve) => {
     const dummy = [
       {
+        profileSrc: '/profile.png',
         name: '이용재',
-        email: 'lee@naver.com',
+        leader: true,
         id: 1,
+        email: 'lee@naver.com',
       },
       {
+        profileSrc: '/profile.png',
         name: '장동균',
-        email: 'jang@gmail.com',
+        leader: false,
         id: 2,
+        email: 'jang@gmail.com',
       },
       {
+        profileSrc: '/profile.png',
         name: '장민호',
-        email: 'minho9301@naver.com',
+        leader: false,
         id: 3,
+        email: 'minho9301@naver.com',
       },
       {
+        profileSrc: '/profile.png',
         name: '강승현',
-        email: 'kangkang@gmail.com',
+        leader: true,
         id: 4,
+        email: 'kangkang@naver.com',
       },
     ];
 
-    const userOptions = dummy.map((item) => {
+    const userOptions: MemberOption[] = dummy.map((item) => {
       return {
         label: `${item.name} (${item.email})`,
-        value: item.name,
-        id: item.id,
+        value: item.id,
+        ...item
       };
     });
 
-    resolve(userOptions.filter((i) => i.value.includes(param)));
+    resolve(userOptions.filter((i) => i.name.includes(param)));
   });
 
   // TODO: api 연결 백엔드 미완.
