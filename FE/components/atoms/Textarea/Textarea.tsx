3b1554b720f37f4f1cf7c646b78512f888efbf68
Dongkyun_Jang <thd6576@naver.com>
2021-08-05 02:24:50 +0900

    fix: textarea를 기존으로 돌립니다

--- FE/components/atoms/Textarea/Textarea.tsx
+++ FE/components/atoms/Textarea/Textarea.tsx
@@ -1,30 +1,16 @@
-import {
-  ReactElement,
-  useEffect,
-  forwardRef,
-  ReactNode,
-  FocusEventHandler,
-} from 'react';
+import { ReactElement, forwardRef, ReactNode, FocusEventHandler } from 'react';
 
 interface TextareaProps {
   children?: ReactNode;
   rows?: number;
   cols?: number;
   placeholder?: string;
-  refValue?: string;
   onBlur: FocusEventHandler;
 }
 
 const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ children, refValue = '', ...rest }: TextareaProps, ref): ReactElement => {
+  ({ children, ...rest }: TextareaProps, ref): ReactElement => {
     // TODO: current에 대한 타입 지정
-    useEffect(() => {
-      if (ref && refValue) {
-        if (ref.current) {
-          ref.current.value = refValue;
-        }
-      }
-    }, []);
 
     return (
       <textarea ref={ref} {...rest}>
