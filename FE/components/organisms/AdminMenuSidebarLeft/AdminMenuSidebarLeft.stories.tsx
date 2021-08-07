00a7b0272c3a40e0cc9a771a28f9480a15de383a
장민호 <minho9301@naver.com>
2021-08-08 08:28:20 +0900

    style: AdminMenuSidebarLeft 스타일 수정에 따라 수정
    flex를 이용하여 AdminMenuSidebarLeft의 너비를 지정해준다

--- FE/components/organisms/AdminMenuSidebarLeft/AdminMenuSidebarLeft.stories.tsx
+++ FE/components/organisms/AdminMenuSidebarLeft/AdminMenuSidebarLeft.stories.tsx
@@ -1,17 +1,32 @@
 import { Story } from '@storybook/react';
 import AdminMenuSidebarLeft from './AdminMenuSidebarLeft';
+import styled from 'styled-components';
 
 export default {
   title: 'Organisms/Admin Menu Sidebar Left',
   component: AdminMenuSidebarLeft,
 };
 
+const Wrapper = styled.div`
+  display: flex;
+
+  > div:nth-child(1) {
+    flex: 0 0 200px;
+  }
+  > div:nth-child(2) {
+    flex: auto;
+  }
+`;
+
 const Template: Story = ({ projects }) => (
-  <AdminMenuSidebarLeft
-    changeMenu={(menuIndex) => console.log(menuIndex)}
-    changeProject={(projectId) => console.log(projectId)}
-    projects={projects}
-  />
+  <Wrapper>
+    <AdminMenuSidebarLeft
+      onChangeMenu={(menuIndex) => console.log(menuIndex)}
+      onChangeProject={(projectId) => console.log(projectId)}
+      projects={projects}
+    />
+    <div></div>
+  </Wrapper>
 );
 
 export const adminMenuSidebarLeft = Template.bind({});
