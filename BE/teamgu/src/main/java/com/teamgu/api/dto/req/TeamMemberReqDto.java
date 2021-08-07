cd2f397d3a223c0e939dadd6511589e19a3d4b52
안석현 <naannaan@naver.com>
2021-08-07 15:37:18 +0900

    feat: add team request dto

---
+++ BE/teamgu/src/main/java/com/teamgu/api/dto/req/TeamMemberReqDto.java
@@ -0,0 +1,18 @@
+package com.teamgu.api.dto.req;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@ApiModel(description = "팀 구성시 멤버 정보 요청 모델")
+public class TeamMemberReqDto {
+
+	@ApiModelProperty(name = "team index", example = "1")
+	Long teamId;
+	
+	@ApiModelProperty(name = "user index", example = "1")
+	Long userId;
+}
