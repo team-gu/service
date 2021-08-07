d9dc01f22ec926fafcea547010f89fb680466f17
장민호 <minho9301@naver.com>
2021-08-08 08:27:29 +0900

    style: AdminMenuSidebar 너비 수정
    AdminMenuSidebar 자체는 너비를 가지지 않고, 부모의 너비를 따라가도록 한다

--- FE/components/organisms/AdminMenuSidebarLeft/AdminMenuSidebarLeft.tsx
+++ FE/components/organisms/AdminMenuSidebarLeft/AdminMenuSidebarLeft.tsx
@@ -4,70 +4,70 @@ import styled from 'styled-components';
 import { Text } from '@atoms';
 import { SimpleSelect } from '@molecules';
 import { Project } from '@utils/type';
+import { ADMIN_MENU_CONTENT } from '@utils/constants';
 
 const Wrapper = styled.div`
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  width: 200px;
+  .sidebar {
+    position: relative;
 
-  display: flex;
-  flex-direction: column;
-  box-shadow: 3px 0px 5px rgba(55, 53, 47, 0.4);
-
-  .sidebar-header {
-    flex: 0 0 100px;
-
-    display: flex;
-    align-items: center;
-
-    border: solid 2px #3848a0;
-    background-color: #e8eaf6;
-
-    .selected-project-info {
-      flex: 1;
-      text-align: center;
-    }
-  }
-
-  .sidebar-content {
-    flex: 1 0 auto;
     display: flex;
     flex-direction: column;
-    gap: 40px;
+    box-shadow: 3px 0px 5px rgba(55, 53, 47, 0.4);
 
-    background-color: #3848a0;
-    color: white;
-    padding-top: 60px;
+    .sidebar-header {
+      flex: 0 0 100px;
 
-    > div {
-      margin-left: 20px;
-      cursor: pointer;
+      display: flex;
+      align-items: center;
+
+      border: solid 2px #3848a0;
+      background-color: #e8eaf6;
+
+      .selected-project-info {
+        flex: 1;
+        text-align: center;
+
+        .current-project-text {
+          cursor: pointer;
+          text-decoration: underline;
+          text-decoration-thickness: 1px;
+          text-underline-offset: 3px;
+        }
+      }
+    }
+
+    .sidebar-content {
+      flex: 1 0 auto;
+      display: flex;
+      flex-direction: column;
+      gap: 40px;
+
+      background-color: #3848a0;
+      color: white;
+      padding: 60px 0;
+
+      > div {
+        margin-left: 20px;
+        cursor: pointer;
+      }
     }
   }
 `;
 
-export const MENU_CONTENT = [
-  '프로젝트 관리',
-  '대시보드',
-  '회원 관리',
-  '팀 관리',
-  '공지사항 관리',
-];
+
 
 interface AdminMenuSidebarLeftProps {
-  changeMenu: (selectedMenu: number) => void;
-  changeProject: (selectedProjectId: number) => void;
+  onChangeMenu: (selectedMenu: number) => void;
+  onChangeProject: (selectedProjectId: number) => void;
   projects: Project[];
 }
 
 export default function AdminMenuSidebarLeft({
-  changeMenu,
-  changeProject,
+  onChangeMenu,
+  onChangeProject,
   projects,
 }: AdminMenuSidebarLeftProps): ReactElement {
-  const menuOptions = MENU_CONTENT.map((v, i) => {
+  const menuOptions = ADMIN_MENU_CONTENT.map((v, i) => {
     return { id: i, title: v, label: v, value: i };
   });
 
@@ -85,16 +85,16 @@ export default function AdminMenuSidebarLeft({
 
   const handleChangeMenu = (index: number) => {
     setSelectedMenu(index);
-    changeMenu(index);
+    onChangeMenu(index);
   };
 
-  const handleChnageSelectProject = () => {
+  const handleChangeSelectProject = () => {
     setClickSelectProject(!clickSelectProject);
   };
 
   const handleChangeProject = (project: Project) => {
     setSelectedProject({ ...project, label: project.name, value: project.id });
-    changeProject(project.id);
+    onChangeProject(project.id);
   };
 
   const customStyles = {
@@ -128,36 +128,38 @@ export default function AdminMenuSidebarLeft({
 
   return (
     <Wrapper>
-      <div className="sidebar-header">
-        <div className="selected-project-info">
-          <Text text="현재 선택된 프로젝트" color="gray" fontSetting="n16m" />
-          {clickSelectProject ? (
-            <SimpleSelect
-              options={projectOptions}
-              onChange={handleChangeProject}
-              onBlur={handleChnageSelectProject}
-              customStyles={customStyles}
-              value={selectedProject}
-            />
-          ) : (
-            <div onClick={handleChnageSelectProject}>
-              <Text text={selectedProject.name} fontSetting="n20m" />
-            </div>
-          )}
-        </div>
-      </div>
-      <div className="sidebar-content">
-        {menuOptions.map(({ id, title }) => (
-          <div onClick={() => handleChangeMenu(id)}>
-            <Text
-              key={id}
-              text={title}
-              fontSetting={id === selectedMenu ? 'n26b' : 'n16m'}
-              color={id === selectedMenu ? 'white' : '#eeeeee'}
-              isLineBreak
-            />
+      <div className="sidebar">
+        <div className="sidebar-header">
+          <div className="selected-project-info">
+            <Text text="현재 선택된 프로젝트" color="gray" fontSetting="n16m" />
+            {clickSelectProject ? (
+              <SimpleSelect
+                options={projectOptions}
+                onChange={handleChangeProject}
+                onBlur={handleChangeSelectProject}
+                customStyles={customStyles}
+                value={selectedProject}
+                autofocus={true}
+              />
+            ) : (
+              <div onClick={handleChangeSelectProject} className="current-project-text">
+                <Text text={selectedProject.name} fontSetting="n20m" />
+              </div>
+            )}
           </div>
-        ))}
+        </div>
+        <div className="sidebar-content">
+          {menuOptions.map(({ id, title }) => (
+            <div key={id} onClick={() => handleChangeMenu(id)}>
+              <Text
+                text={title}
+                fontSetting={id === selectedMenu ? 'n26b' : 'n16m'}
+                color={id === selectedMenu ? 'white' : '#eeeeee'}
+                isLineBreak
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </Wrapper>
   );
