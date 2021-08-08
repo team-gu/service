d43192f0c0a5628d0768b1c3e7fef95253cc1946
장민호 <minho9301@naver.com>
2021-08-08 14:49:37 +0900

    feat: 팀 페이지 관련 API 연동
    기존에 더미데이터를 반환하던 것을 실제 서버로 요청을 보내도록 수정

--- FE/repository/teamRepository.ts
+++ FE/repository/teamRepository.ts
@@ -1,163 +1,63 @@
 import api from '@context/serverContext';
-import { Team } from '@utils/type';
 
 export const getTeams = async (
   sortBy: string,
   sortAsc: boolean,
   containsUserId?: number,
 ) => {
-  console.log('팀 가져오는 API 호출');
+  console.log('팀 조회 API 호출');
   console.log('정렬 기준 :', sortBy);
   console.log('오름차순 :', sortAsc);
   console.log('포함하는 사용자 아이디 :', containsUserId);
 
-  return new Promise<Team[]>((resolve) => {
-    const dummy = [
-      {
-        id: 1,
-        name: '웹기술 할거에요',
-        introduce: '현재 TEAM 테이블에 팀 소개 컬럼이 존재하지 않습니다',
-        completeYN: 0,
-        nowNumber: 2,
-        maxNumber: 3,
-        leaderId: 19,
-        trackName: '웹 기술',
-        teamMembers: [
-          {
-            id: 19,
-            name: '장민호',
-            img: '/profile.png',
-            email: 'naannaan@naver.com',
-          },
-          {
-            id: 3,
-            name: '박싸피',
-            img: null,
-            email: 'parkssafy@naver.com',
-          },
-          {
-            id: 6,
-            name: 'idea',
-            img: null,
-            email: 'idea@naver.com',
-          },
-        ],
-        skills: [
-          {
-            id: 1,
-            name: 'React',
-          },
-          {
-            id: 2,
-            name: 'Spring',
-          },
-          {
-            id: 3,
-            name: 'MySQL',
-          },
-          {
-            id: 4,
-            name: 'WebRTC',
-          },
-        ],
-      },
-      {
-        id: 2,
-        name: '웹 디자인 할거에요',
-        introduce: '현재 TEAM 테이블에 팀 소개 컬럼이 존재하지 않습니다',
-        completeYN: 0,
-        nowNumber: 2,
-        maxNumber: 3,
-        leaderId: 2,
-        trackName: '웹 디자인',
-        teamMembers: [
-          {
-            id: 2,
-            name: '김싸피',
-            img: null,
-            email: 'kimssafy@naver.com',
-          },
-          {
-            id: 4,
-            name: '강싸피',
-            img: null,
-            email: 'kangssafy@naver.com',
-          },
-        ],
-        skills: [
-          {
-            id: 5,
-            name: 'JPA',
-          },
-          {
-            id: 6,
-            name: 'HTML',
-          },
-          {
-            id: 7,
-            name: 'CSS',
-          },
-          {
-            id: 8,
-            name: 'JavaScript',
-          },
-        ],
-      },
-    ];
-
-    resolve(dummy);
+  // TODO: 필터링 추가
+  return await api({
+    url: `/api/team`,
+    type: 'get',
   });
-  // TODO: api 연결 백엔드 미완.
-  // return await api({
-  //   url: `/path/to/get-teams`,
-  //   type: 'get',
-  // });
 };
 
-export const createTeam = async (param: object) => {
-  console.log('팀 생성 API 호출: ');
-  console.log(param);
+export const getUserTeamIn = async (param: any) => {
+  return await api({
+    url: `/path/to/get-user-team-in/${param}`,
+    type: 'get',
+  })
+}
 
-  // TODO: api 연결 백엔드 미완.
-  // return await api({
-  //   url: `/path/to/create-team`,
-  //   type: 'post',
-  //   param,
-  // });
+export const createTeam = async (param: any) => {
+  console.log('create team: ', param);
+  return await api({
+    url: `/api/team/add`,
+    type: 'post',
+    param,
+  });
 };
 
-export const updateTeam = async (param: object) => {
-  console.log('팀 수정 API 호출: ');
-  console.log(param);
-
-  // TODO: api 연결 백엔드 미완.
-  // return await api({
-  //   url: `/path/to/create-team`,
-  //   type: 'post',
-  //   param,
-  // });
+export const updateTeam = async (param: any) => {
+  console.log('update team: ', param);
+  return await api({
+    url: `/api/team/${param.id}`,
+    type: 'put',
+    param,
+  });
 };
 
-export const deleteTeam = async (param: object) => {
-  console.log('팀 삭제 API 호출: ');
-  console.log(param);
-
-  // TODO: api 연결 백엔드 미완.
-  // return await api({
-  //   url: `/path/to/delete-team`,
-  //   type: 'post',
-  //   param,
-  // });
+export const deleteTeam = async (param: any) => {
+  console.log('update team: ', param);
+  return await api({
+    url: `/api/team/${param.id}`,
+    type: 'delete',
+  });
 };
 
-export const exitTeam = async (param: object) => {
+export const exitTeam = async (param: any) => {
   console.log('팀 탈퇴 API 호출: ');
   console.log(param);
 
   // TODO: api 연결 백엔드 미완.
   // return await api({
-  //   url: `/path/to/exit-team`,
-  //   type: 'post',
+  //   url: `/api/team/exit-team`,
+  //   type: 'delete',
   //   param,
   // });
 };
