bedcc8e1f29f10a00a10152e6bb9c6305b32d072
dididy <dev.yongjaelee@gmail.com>
2021-08-11 05:47:30 +0900

    refactor: Floating 버튼에 전체노티를 보여주는 아이콘을 추가한다
    * 노티갯수 노출

--- FE/components/molecules/FloatingButton/FloatingButton.tsx
+++ FE/components/molecules/FloatingButton/FloatingButton.tsx
@@ -1,13 +1,16 @@
-import { ReactElement, MouseEventHandler } from 'react';
+import { ReactElement, MouseEventHandler, useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 
 import styled from 'styled-components';
 
+import { Notification } from '@molecules';
 import { Icon } from '@atoms';
+import { getNotificationNumber } from '@repository/chatRepository';
 
 interface FloatingButtonProps {
   func?: MouseEventHandler<HTMLSpanElement>;
   isRtc?: boolean;
+  id: number;
 }
 
 const Wrapper = styled(motion.div)<{ isRtc: boolean }>`
@@ -48,16 +51,46 @@ const Wrapper = styled(motion.div)<{ isRtc: boolean }>`
     :hover {
       opacity: 0.8;
     }
+    .notification {
+      position: absolute;
+      top: 0;
+      right: 0;
+    }
   `}
 `;
 
 export default function FloatingButton({
   func = () => {},
   isRtc = false,
+  id,
 }: FloatingButtonProps): ReactElement {
+  const [unreadCount, setUnreadCount] = useState(0);
+  const handleGetNotificationNumber = async () => {
+    try {
+      if (id !== 0) {
+        const {
+          data: {
+            data: { unreadcount },
+          },
+        } = await getNotificationNumber(id);
+        setUnreadCount(unreadcount);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    handleGetNotificationNumber();
+    const interval = setInterval(() => {
+      handleGetNotificationNumber();
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
   return (
     <Wrapper onClick={func} whileTap={{ rotate: 90, scale: 0.9 }} isRtc={isRtc}>
       <Icon iconName="message" size="24px" />
+      {!isRtc && <Notification alertNumber={unreadCount} />}
     </Wrapper>
   );
 }
