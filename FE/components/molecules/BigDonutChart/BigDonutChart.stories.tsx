7152a4d60e8d49a305c1013933168f918bb29df5
장민호 <minho9301@naver.com>
2021-08-10 17:34:51 +0900

    fix: 도넛 차트 스토리 수정, 맨 끝라인 줄바꿈 추가

--- FE/components/molecules/BigDonutChart/BigDonutChart.stories.tsx
+++ FE/components/molecules/BigDonutChart/BigDonutChart.stories.tsx
@@ -1,16 +1,40 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { Story } from '@storybook/react';
 import BigDonutChart from './BigDonutChart';
 
-import { ENTIRE_TEAM_DATA } from '@utils/dummy';
+import { ADMIN_TEAM_DATA } from '@utils/dummy';
+
+const BLUE = '#0088FE';
+const RED = '#FF8042';
 
 export default {
   title: 'Molecules/Big Donut Chart',
   component: BigDonutChart,
 };
 
-const Template: Story = () => (
-  <BigDonutChart data={ENTIRE_TEAM_DATA} title="제목" />
-);
+const Template: Story = () => {
+  const [regionTeamData, setRegionTeamData] = useState<any[]>();
+  // init data
+  useEffect(() => {
+    const tmp = ADMIN_TEAM_DATA.map((d) => ({
+      title: d.title,
+      data: d.data.map((item) => ({
+        ...item,
+        color: item.name === '팀 완성' ? BLUE : RED,
+      })),
+    }));
+    setRegionTeamData(tmp);
+  }, []);
+  return (
+    <>
+      {regionTeamData && regionTeamData.length > 0 && (
+        <BigDonutChart
+          data={regionTeamData[0].data}
+          title={regionTeamData[0].title}
+        />
+      )}
+    </>
+  );
+};
 
 export const bigDonutChart = Template.bind({});
