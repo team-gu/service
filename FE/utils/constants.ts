2a610944d0b4a0a59697e8cc479df6901ee6d820
Dongkyun_Jang <thd6576@naver.com>
2021-08-12 11:02:12 +0900

    feat: 이미지를 불러오는 함수를 정의 합니다
    뒤에 브라우저 캐싱을 막기 위해 # new Date().getTime()을 추가합니다. 해당
    문구를 통해 uri가 같더라도 다시 가져오게 됩니다. 하지만 매번 다시 이미지
    파일을 가져오기 때문에 순간 깜빡이는 현상이 발생합니다. 하지만, 현재
    선택할 수 있는 방법 중에서는 이게 가장 최선인 것 같습니다.

--- FE/utils/constants.ts
+++ FE/utils/constants.ts
@@ -50,5 +50,13 @@ export const VIDEO_CHAT_PATH_PREFIX = '/rtc';
 export const CODE_ID = {
   기수: 'ST',
   트랙: 'TR',
-  구분: 'PR'
-}
+  구분: 'PR',
+};
+
+export const getImageURL = (image: string) => {
+  if (image === '' || image === 'null.null') return '/profile.png';
+  return (
+    `https://i5a202.p.ssafy.io:8080/api/file/display?url=profile/${image}#` +
+    new Date().getTime()
+  );
+};
