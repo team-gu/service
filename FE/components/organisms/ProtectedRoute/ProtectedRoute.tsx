fd85e8be0f803b154c4238b5a8525caeb72d6e66
Dongkyun_Jang <thd6576@naver.com>
2021-08-13 02:43:30 +0900

    feat: ProtectedRRoute를 정의합니다
    1. 토큰이 있지만 상태값이 없는 경우 상태값을 가져옵니다
    2. 페이지 전환시에 스피너를 작동시킵니다
    3. 로그인하지 않은 유저가 다른 페이지로 이동하는 것을 막습니다

--- FE/components/organisms/ProtectedRoute/ProtectedRoute.tsx
+++ FE/components/organisms/ProtectedRoute/ProtectedRoute.tsx
@@ -1,3 +1,59 @@
+import { useEffect, useState } from 'react';
+import Router, { useRouter } from 'next/router';
+import { useAppDispatch, useAuthState, setLoading, setUser } from '@store';
+import { saveItem, loadItem } from '@utils/storage';
+import { getUserDetailbyRefresh } from '@repository/userprofile';
+
+interface ProtectedRouteProps {
+  children: JSX.Element;
+}
+
+export default function ProtectedRoute({ children }: ProtectedRouteProps) {
+  const { user } = useAuthState();
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+  const [flag, setFlag] = useState(false);
+
+  useEffect(() => {
+    const accessToken = loadItem('accessToken');
+    if (accessToken && !user.name) {
+      (async () => {
+        try {
+          const path = router.route;
+          const {
+            data: { data },
+          } = await getUserDetailbyRefresh();
+          saveItem('accessToken', data.accessToken);
+          dispatch(setUser(data.userInfo));
+          router.push(path);
+        } catch (e) {
+          console.error(e);
+        }
+      })();
+    } else {
+      setFlag(true);
+      router.push('/');
+    }
+  }, []);
+
+  useEffect(() => {
+    const start = () => {
+      dispatch(setLoading({ isLoading: true }));
+    };
+    const end = () => {
+      dispatch(setLoading({ isLoading: false }));
+    };
+    Router.events.on('routeChangeStart', start);
+    Router.events.on('routeChangeComplete', end);
+    Router.events.on('routeChangeError', end);
+
+    return () => {
+      Router.events.off('routeChangeStart', start);
+      Router.events.off('routeChangeComplete', end);
+      Router.events.off('routeChangeError', end);
+    };
+  }, []);
+
   useEffect(() => {
     const path = router.route;
     const requireAuthPath = [
@@ -15,3 +71,9 @@
       }
     }
   });
+
+  if (user.name || flag) {
+    return children;
+  }
+  return null;
+}
