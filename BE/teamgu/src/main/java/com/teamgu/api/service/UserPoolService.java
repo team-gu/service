8fa64991af9db557aa08b2187b089ba445a6263a
donggillee-dev <donggillee94@gmail.com>
2021-08-12 19:09:53 +0900

    feat : 인력풀 유저 검색 자동완성 기능 추가
    - email / 이름 둘 다 검색 가능하게끔 처리
    - img가 비어있을 경우 null로 리턴
    - img존재할 경우에는 (서버에 저장된 파일명).(확장자) 형식으로 보낸다
    
    > resolve : #S05P13A202-172

--- BE/teamgu/src/main/java/com/teamgu/api/service/UserPoolService.java
+++ BE/teamgu/src/main/java/com/teamgu/api/service/UserPoolService.java
@@ -1,10 +1,13 @@
 package com.teamgu.api.service;
 
+import com.teamgu.api.dto.req.UserPoolNameReqDto;
 import com.teamgu.api.dto.req.UserPoolReqDto;
+import com.teamgu.api.dto.res.UserPoolNameResDto;
 import com.teamgu.api.dto.res.UserPoolResDto;
 
 import java.util.List;
 
 public interface UserPoolService {
     List<UserPoolResDto> findUsersByFilter(UserPoolReqDto userPoolReqDto);
+    List<UserPoolNameResDto> findUsersBySimName(UserPoolNameReqDto userPoolNameReqDto);
 }
--- BE/teamgu/src/main/java/com/teamgu/database/repository/UserPoolRepositoryCustom.java
+++ BE/teamgu/src/main/java/com/teamgu/database/repository/UserPoolRepositoryCustom.java
@@ -1,6 +1,8 @@
 package com.teamgu.database.repository;
 
+import com.teamgu.api.dto.req.UserPoolNameReqDto;
 import com.teamgu.api.dto.req.UserPoolReqDto;
+import com.teamgu.api.dto.res.UserPoolNameResDto;
 
 import java.util.List;
 
@@ -9,4 +11,5 @@ import java.util.List;
  */
 public interface UserPoolRepositoryCustom {
     List<Object[]> findUsersByFilter(UserPoolReqDto userPoolReqDto);
+    List<UserPoolNameResDto> findUsersBySimName(UserPoolNameReqDto userPoolNameReqDto);
 }
