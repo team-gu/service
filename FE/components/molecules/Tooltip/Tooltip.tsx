1813789bb7c7d9add8a8acf929777065e3efdd2d
dididy <dev.yongjaelee@gmail.com>
2021-08-12 11:57:58 +0900

    fix: Tooltip 스타일을 적절한 방향으로 수정한다
    * z-index 제거, position relative 추가 등

--- FE/components/molecules/Tooltip/Tooltip.tsx
+++ FE/components/molecules/Tooltip/Tooltip.tsx
@@ -6,26 +6,22 @@ interface TooltipProps {
 }
 
 const Wrapper = styled.div<{ isOpen: boolean }>`
-  ${({ isOpen }) =>
-    isOpen &&
-    `
-    fill: #0b3895;
-    transform: scale(1);
-  `}
+  position: relative;
 
   .content {
+    ${({ theme: { flexRow } }) => flexRow()}
     ${({ isOpen }) => !isOpen && 'display: none !important;'}
 
     position: absolute;
     top: -40px;
     right: -35px;
-    z-index: 200;
+
     background-color: white;
     box-shadow: 0 6px 12px 0 rgba(4, 4, 161, 0.04);
+
     padding: 10px;
     border-radius: 5px;
 
-    ${({ theme: { flexRow } }) => flexRow()}
     input {
       height: 100%;
     }
