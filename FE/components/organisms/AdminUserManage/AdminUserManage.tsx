48fcb9f9a71d24499f160c1ec8168225203f16fc
장민호 <minho9301@naver.com>
2021-08-14 21:45:19 +0900

    feat: 회원 정보 추가 모달 띄우기 UI

--- FE/components/organisms/AdminUserManage/AdminUserManage.tsx
+++ FE/components/organisms/AdminUserManage/AdminUserManage.tsx
@@ -1,14 +1,26 @@
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
-import { Text } from '@atoms';
+import { Text, Icon } from '@atoms';
 import { ReactTable } from '@molecules';
+import { AdminUserManageModal } from '@organisms';
 import { getTableData } from '@repository/adminRepository';
 import { ADMIN_DASHBOARD_TABLE_COLUMNS } from '@utils/constants';
 
 const Wrapper = styled.div`
+  i {
+    cursor: pointer;
+  }
   .manage-header {
+    display: flex;
+    align-items: center;
     margin: 20px 0;
+    > div {
+      margin-right: 10px;
+    }
+    > i {
+      font-size: 30px;
+    }
   }
 `;
 
@@ -18,6 +30,7 @@ interface AdminUserManageProps {
 
 export default function AdminUserManage({ projectId }: AdminUserManageProps) {
   const [teamStatusTableData, setTeamStatusTableData] = useState<any[]>([]);
+  const [showCreateModal, setCreateShowModal] = useState(false);
 
   useEffect(() => {
     getTableData({ projectId }).then(({ data: { data } }) => {
@@ -29,12 +42,16 @@ export default function AdminUserManage({ projectId }: AdminUserManageProps) {
     <Wrapper>
       <div className="manage-header">
         <Text text="교육생 목록" fontSetting="n26b" />
+        <Icon iconName="add_box" func={() => setCreateShowModal(true)} />
       </div>
       <ReactTable
         data={teamStatusTableData}
         columns={ADMIN_DASHBOARD_TABLE_COLUMNS}
         fullWidth={false}
       />
+      {showCreateModal && (
+        <AdminUserManageModal handleClickClose={() => setCreateShowModal(false)} />
+      )}
     </Wrapper>
   );
 }
