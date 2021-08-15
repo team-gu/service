afe6baedd1c5c9087fe6388f49d5a2bee5d92c30
dididy <dev.yongjaelee@gmail.com>
2021-08-16 06:59:33 +0900

    fix: ChatLoading 컴포넌트 및  스토리를 제작한다
    * 로딩 애니메이션

---
+++ FE/components/molecules/ChatLoading/ChatLoading.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Story } from '@storybook/react';
+import ChatLoading from './ChatLoading';
+
+export default {
+  title: 'Molecules/Chat Loading',
+  component: ChatLoading,
+};
+
+const Template: Story = () => <ChatLoading />;
+
+export const checkbox = Template.bind({});
+
+checkbox.args = {};
