d67eb8557df786fefa1347cd993dec4d27a18278
Dongkyun_Jang <thd6576@naver.com>
2021-08-15 17:30:54 +0900

    feat: 비밀번호 초기화 모달
    입력한 이메일로 임의의 문자열이 가게 됩니다. 사용자의 아이디는 실제
    사용자가 사용하는 이메일이라는 가정이 가능하기 때문에 이런 식으로
    설계하였습니다. 악의적으로 다른 사용자의 이메일을 통해 비밀번호를
    변경시키는 경우도 발생할 수 있고 해당 부분을 백엔드에서
    처리해주셨습니다.

---
+++ FE/components/organisms/Main/Modal/FindPasswordModal.tsx
@@ -0,0 +1,89 @@
+import { ReactElement, SyntheticEvent, useState, useRef } from 'react';
+import styled from 'styled-components';
+import { Input } from '@atoms';
+import { Button } from '@molecules';
+import { findPassword } from '@repository/userprofile';
+import { useAppDispatch, removeModal } from '@store';
+import { MODALS } from '@utils/constants';
+
+const Wrapper = styled.div`
+  margin: 30px;
+`;
+
+const FindPW = styled.div`
+  div {
+    margin-bottom: 15px;
+
+    button {
+      width: 5vw;
+      height: 20px;
+      margin-left: 5px;
+    }
+  }
+`;
+
+const Error = styled.div`
+  font-weight: 900;
+  color: red;
+`;
+
+export default function FindPasswordModal(): ReactElement {
+  const dispatch = useAppDispatch();
+
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const emailRef = useRef<HTMLInputElement>(null);
+
+  const handlePassword = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    if (!emailRef?.current?.value) {
+      setErrorMessage('이메일을 입력해주세요.');
+      setError(true);
+      return;
+    }
+
+    try {
+      const data = {
+        email: emailRef?.current?.value,
+      };
+      console.log(data);
+      await findPassword(data);
+      alert('비밀번호가 초기화 되었습니다.');
+      dispatch(removeModal({ modalName: MODALS.FINDPASSWORD_MODAL }));
+    } catch (e) {
+      console.log(e);
+      setErrorMessage('올바르지 않은 이메일입니다.');
+      setError(true);
+    }
+  };
+
+  return (
+    <Wrapper>
+      <form onSubmit={handlePassword}>
+        <FindPW>
+          <div>
+            <Input
+              placeHolder={'이메일을 입력해주세요.'}
+              width="30vw"
+              height="50px"
+              ref={emailRef}
+              maxLength={30}
+              autoComplete="off"
+            />
+          </div>
+          {error && <Error>{errorMessage}</Error>}
+          <div>
+            <Button title="확인" type="submit" />
+            <Button
+              title="닫기"
+              func={() =>
+                dispatch(removeModal({ modalName: MODALS.FINDPASSWORD_MODAL }))
+              }
+            />
+          </div>
+        </FindPW>
+      </form>
+    </Wrapper>
+  );
+}
