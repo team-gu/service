3d339c9e1fbadea5f76a63cbb03f9586677b175f
dididy <dev.yongjaelee@gmail.com>
2021-08-15 11:17:48 +0900

    fix: user관련 변경된 엔드포인트 주소를 반영한다
    * reqInfo 제외하고 전부 auth에서 user로 변경

--- FE/repository/userprofile.ts
+++ FE/repository/userprofile.ts
@@ -3,47 +3,47 @@ import api from '@context/serverContext';
 //TODO 중복이 너무 많기 때문에 두 개씩 하나의 함수로 묶는 리팩토링이 필요합니다.
 export const postProject = async (param: object) =>
   await api({
-    url: '/api/auth/project',
+    url: '/api/user/project',
     type: 'post',
     param,
   });
 
 export const updateProject = async (param: object) =>
   await api({
-    url: '/api/auth/project',
+    url: '/api/user/project',
     type: 'put',
     param,
   });
 
 export const deleteProject = async (id: number) =>
   await api({
-    url: `/api/auth/project/${id}`,
+    url: `/api/user/project/${id}`,
     type: 'delete',
   });
 
 export const postAward = async (param: object) =>
   await api({
-    url: '/api/auth/award',
+    url: '/api/user/award',
     type: 'post',
     param,
   });
 
 export const updateAward = async (param: object) =>
   await api({
-    url: '/api/auth/award',
+    url: '/api/user/award',
     type: 'put',
     param,
   });
 
 export const deleteAward = async (id: number) =>
   await api({
-    url: `/api/auth/award/${id}`,
+    url: `/api/user/award/${id}`,
     type: 'delete',
   });
 
 export const updateDetailInformation = async (param: object) => {
   return await api({
-    url: '/api/auth/userInfo',
+    url: '/api/user/userInfo',
     type: 'put',
     param,
   });
@@ -51,7 +51,7 @@ export const updateDetailInformation = async (param: object) => {
 
 export const getUserDetail = async (id: string | string[] | undefined) => {
   return await api({
-    url: `/api/auth/userInfo/${id}`,
+    url: `/api/user/userInfo/${id}`,
     type: 'get',
   });
 };
@@ -65,7 +65,7 @@ export const getUserDetailbyRefresh = async () => {
 
 export const changePassword = async (param: object) => {
   await api({
-    url: '/api/auth/password',
+    url: '/api/user/password',
     type: 'put',
     param,
   });
