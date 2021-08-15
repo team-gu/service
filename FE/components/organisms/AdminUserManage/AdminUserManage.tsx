72a7afa0c58aad87bd40860265e89546a5512ba2
장민호 <minho9301@naver.com>
2021-08-15 17:34:40 +0900

    feat: 테이블 행 선택 가능
    prop을 통해 행 선택이 가능하게 하며, 선택 시 실행할 콜백함수도 같이 넣어준다.

--- FE/components/organisms/AdminUserManage/AdminUserManage.tsx
+++ FE/components/organisms/AdminUserManage/AdminUserManage.tsx
@@ -31,6 +31,7 @@ interface AdminUserManageProps {
 export default function AdminUserManage({ projectId }: AdminUserManageProps) {
   const [teamStatusTableData, setTeamStatusTableData] = useState<any[]>([]);
   const [showCreateModal, setCreateShowModal] = useState(false);
+  const [editable, setEditable] = useState(false);
 
   useEffect(() => {
     getTableData({ projectId }).then(({ data: { data } }) => {
@@ -43,14 +44,22 @@ export default function AdminUserManage({ projectId }: AdminUserManageProps) {
       <div className="manage-header">
         <Text text="교육생 목록" fontSetting="n26b" />
         <Icon iconName="add_box" func={() => setCreateShowModal(true)} />
+        <Icon
+          iconName="settings_applications"
+          func={() => setEditable(!editable)}
+        />
       </div>
       <ReactTable
         data={teamStatusTableData}
         columns={ADMIN_DASHBOARD_TABLE_COLUMNS}
         fullWidth={false}
+        selectable={editable}
+        onSelectRow={(row) => console.log(row)}
       />
       {showCreateModal && (
-        <AdminUserManageModal handleClickClose={() => setCreateShowModal(false)} />
+        <AdminUserManageModal
+          handleClickClose={() => setCreateShowModal(false)}
+        />
       )}
     </Wrapper>
   );
