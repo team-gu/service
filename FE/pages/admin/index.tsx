716e031f35a589d9199c632dd987831fad33759c
장민호 <minho9301@naver.com>
2021-08-18 10:39:45 +0900

    feat: 적절한 Role을 가지고 있지 않으면 관리자 페이지를 볼 수 없음

--- FE/pages/admin/index.tsx
+++ FE/pages/admin/index.tsx
@@ -1,10 +1,22 @@
 import { ReactElement } from 'react';
 import { AdminLayout } from '@organisms';
+import { useAuthState } from '@store';
 
 export default function Admin(): ReactElement {
+  const { user : { role }} = useAuthState();
+
   return (
     <>
-      <AdminLayout />
+      {role !== 3 && role !== 4 ? (
+        <div style={{
+          padding: '50px',
+        }}>
+          권한이 없습니다. 다시 로그인 한 후에 시도해주세요.
+        </div>
+      ) : (
+        <AdminLayout />
+      )}
+      
     </>
   );
 }
