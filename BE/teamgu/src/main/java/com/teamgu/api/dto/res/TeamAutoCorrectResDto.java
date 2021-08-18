6755ccfcb926901a880b4d7f28b39ac384f909d3
안석현 <naannaan@naver.com>
2021-08-18 16:22:54 +0900

    fix: 팀풀 자동완성 수정

--- BE/teamgu/src/main/java/com/teamgu/api/dto/res/TeamAutoCorrectResDto.java
+++ BE/teamgu/src/main/java/com/teamgu/api/dto/res/TeamAutoCorrectResDto.java
@@ -2,13 +2,16 @@ package com.teamgu.api.dto.res;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
-@Setter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 @ApiModel(description = "자동 검색 응답 모델")
 public class TeamAutoCorrectResDto {
 	
@@ -20,11 +23,5 @@ public class TeamAutoCorrectResDto {
 	
 	@ApiModelProperty(name = "사용자 email", example = "naannaan@naver.com")
 	String email;
-
-	public TeamAutoCorrectResDto(Long id, String name, String email) {
-		this.id = id;
-		this.name = name;
-		this.email = email;
-	}
 	
 }
