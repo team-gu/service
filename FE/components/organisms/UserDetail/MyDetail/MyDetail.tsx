724261209759de3383876b7af60c77e48f190a31
dididy <dev.yongjaelee@gmail.com>
2021-08-18 14:43:52 +0900

    fix: MyDetail 컴포넌트의 디자인을 대폭 수정한다
    * 탭 디자인 반영
    * styled 중첩은 하나의 wrapper 관리하도록 수정

--- FE/components/organisms/UserDetail/MyDetail/MyDetail.tsx
+++ FE/components/organisms/UserDetail/MyDetail/MyDetail.tsx
@@ -1,8 +1,8 @@
-import { ReactElement } from 'react';
+import { ReactElement, useState } from 'react';
 import styled from 'styled-components';
-import Image from 'next/image';
-import { Icon } from '@atoms';
-import { SkillSelectAutoComplete, Label } from '@molecules';
+
+import { SkillSelectAutoComplete, Label, ProfileImage } from '@molecules';
+import { Icon, Text, Textarea } from '@atoms';
 import { useAuthState } from '@store';
 import { getImageURL } from '@utils/constants';
 
@@ -20,184 +20,225 @@ interface Award {
   introduce: string;
 }
 
-const Wrapper = styled.div`
-  border: 1px solid rgba(0, 0, 0, 0.2);
+const Wrapper = styled.div<{ isProject: boolean }>`
   width: 60vw;
+  height: fit-content;
   margin: 0 auto 20px auto;
-  border-radius: 3px;
-  box-shadow: 2px 2px 8px black;
+
+  .profile-container {
+    ${({ theme: { flexCol } }) => flexCol('space-between')}
+    height: 150px;
+    margin: 30px 0px;
+  }
+
+  .button-container {
+    ${({ theme: { flexRow } }) => flexRow()}
+    button {
+      width: 50%;
+      height: 50px;
+      border: none;
+      font-size: 14px;
+      font-weight: bold;
+      :first-child {
+        ${({
+          isProject,
+          theme: {
+            colors: { samsungLightBlue },
+          },
+        }) =>
+          !isProject
+            ? `background-color: ${samsungLightBlue};color: white;`
+            : `background-color: white`}
+      }
+      :last-child {
+        ${({
+          isProject,
+          theme: {
+            colors: { samsungLightBlue },
+          },
+        }) =>
+          isProject
+            ? `background-color: ${samsungLightBlue};color: white;`
+            : `background-color: white`}
+      }
+    }
+
+    margin: 20px 0px;
+  }
+
+  .typography {
+    background-color: white;
+    padding: 50px;
+
+    .icons {
+      ${({ theme: { flexRow } }) => flexRow('flex-end')}
+
+      i {
+        padding-left: 10px;
+        cursor: pointer;
+      }
+    }
+
+    .introduction {
+      margin: auto;
+      display: grid;
+      grid-template-columns: 0.8fr 1.2fr;
+      grid-template-areas: 'manifesto profileImage';
+      gap: 40px;
+      @media only all and (max-width: 768px) {
+        grid-template-columns: 1fr;
+        grid-template-areas:
+          'profileImage'
+          'manifesto';
+      }
+
+      .portrait {
+        width: 100% auto;
+        margin-right: 15px;
+        img {
+          border-radius: 50%;
+        }
+        .basicInfo {
+          margin-bottom: 20px;
+        }
+        .track {
+          margin-bottom: 20px;
+        }
+        .position {
+          margin-bottom: 20px;
+        }
+        .skills {
+          margin-bottom: 20px;
+          width: 100%;
+        }
+        .introduce {
+          margin-bottom: 20px;
+        }
+      }
+
+      .manifesto {
+        p {
+          font-size: 20px;
+          line-height: 1.4;
+          width: 80%;
+          & + * {
+            margin-top: 1em;
+          }
+        }
+      }
+    }
+
+    .projects {
+      display: grid;
+      grid-gap: 20px;
+      grid-template-columns: 1fr 1fr 1fr;
+      grid-template-areas: 'project project project';
+
+      @media only all and (max-width: 768px) {
+        grid-template-columns: 1fr 1fr;
+        grid-template-areas: 'project project';
+      }
+
+      a {
+        text-decoration: none;
+        color: black;
+      }
+      p {
+        grid-column: span 3;
+        font-size: 20px;
+        font-weight: 600;
+        line-height: 1.4;
+        width: 80%;
+      }
+
+      .project {
+        position: relative;
+        border: 1px solid #eaeaea;
+        padding: 24px;
+        border-radius: 5px;
+        text-align: left;
+        height: 80px;
+        cursor: pointer;
+        flex: 1.1;
+        transition: box-shadow 0.2s;
+        overflow: hidden;
+        transition: 0.5s;
+        &:hover {
+          box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.1);
+          height: 80%;
+        }
+        .top {
+          display: flex;
+          font-size: 10px;
+          margin-bottom: 16px;
+        }
+        .introduce {
+          position: absolute;
+        }
+      }
+    }
+
+    .awards {
+      display: grid;
+      grid-gap: 20px;
+      grid-template-columns: 1fr 1fr 1fr;
+      grid-template-areas: 'award award award';
+
+      @media only all and (max-width: 768px) {
+        grid-template-columns: 1fr 1fr;
+        grid-template-areas: 'award award';
+      }
+
+      p {
+        grid-column: span 3;
+        font-size: 20px;
+        font-weight: 600;
+        line-height: 1.4;
+        width: 80%;
+      }
+
+      .award {
+        position: relative;
+        border: 1px solid #eaeaea;
+        padding: 24px;
+        border-radius: 5px;
+        text-align: left;
+        height: 80px;
+        cursor: pointer;
+        flex: 1.1;
+        transition: box-shadow 0.2s;
+        overflow: hidden;
+        transition: 0.5s;
+        &:hover {
+          box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.1);
+          height: 80%;
+        }
+
+        .top {
+          display: flex;
+          font-size: 10px;
+          margin-bottom: 15px;
+        }
+        .middle {
+          margin-bottom: 8px;
+          font-size: 8px;
+        }
+      }
+    }
+  }
 
   .name {
     font-size: 20px;
     font-weight: 600;
     line-height: 1.4;
-    margin: 50px;
-  }
-`;
-
-const Icons = styled.div`
-  margin: 30px;
-  display: flex;
-  justify-content: flex-end;
-
-  i {
-    padding-left: 10px;
-    cursor: pointer;
-  }
-`;
-
-const Introduction = styled.div`
-  margin: auto;
-  display: grid;
-  grid-template-columns: 1fr 0.5fr;
-  grid-template-areas: 'manifesto profileImage';
-  gap: 40px;
-  @media only all and (max-width: 768px) {
-    grid-template-columns: 1fr;
-    grid-template-areas:
-      'profileImage'
-      'manifesto';
-  }
-`;
-
-const StyledLabel = styled(Label)`
-  margin-left: 50px;
-`;
-
-const Manifesto = styled.div`
-  p {
-    font-size: 20px;
-    font-weight: 600;
-    line-height: 1.4;
-    width: 80%;
-    & + * {
-      margin-top: 1em;
-    }
-  }
-  .basicInfo {
-    margin: 50px;
-  }
-  .track {
-    margin: 50px;
-  }
-  .position {
-    margin: 50px;
-  }
-  .skills {
-    margin: 50px;
-    width: 50%;
-  }
-  .introduce {
-    margin: 50px;
-  }
-`;
-
-const Portrait = styled.div`
-  width: 100% auto;
-  margin-top: 20vh;
-  margin-right: 15px;
-  img {
-    border-radius: 50%;
-  }
-`;
-
-const Projects = styled.div`
-  display: grid;
-  grid-gap: 20px;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-template-areas: 'project project project';
-  margin: 50px;
-
-  @media only all and (max-width: 768px) {
-    grid-template-columns: 1fr 1fr;
-    grid-template-areas: 'project project';
   }
 
-  a {
-    text-decoration: none;
-    color: black;
-  }
-  p {
-    grid-column: span 3;
-    font-size: 20px;
-    font-weight: 600;
-    line-height: 1.4;
-    width: 80%;
-  }
-`;
-
-const Awards = styled.div`
-  display: grid;
-  grid-gap: 20px;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-template-areas: 'award award award';
-  margin: 50px;
-
-  @media only all and (max-width: 768px) {
-    grid-template-columns: 1fr 1fr;
-    grid-template-areas: 'award award';
-  }
-
-  p {
-    grid-column: span 3;
-    font-size: 20px;
-    font-weight: 600;
-    line-height: 1.4;
-    width: 80%;
-  }
-`;
-
-const Project = styled.div`
-  position: relative;
-  border: 1px solid #eaeaea;
-  padding: 24px;
-  border-radius: 5px;
-  text-align: left;
-  height: 80px;
-  cursor: pointer;
-  flex: 1.1;
-  transition: box-shadow 0.2s;
-  overflow: hidden;
-  transition: 0.5s;
-  &:hover {
-    box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.1);
-    height: 80%;
-  }
-  .top {
-    display: flex;
-    font-size: 10px;
-    margin-bottom: 16px;
-  }
-  .introduce {
-    position: absolute;
-  }
-`;
-
-const Award = styled.div`
-  position: relative;
-  border: 1px solid #eaeaea;
-  padding: 24px;
-  border-radius: 5px;
-  text-align: left;
-  height: 80px;
-  cursor: pointer;
-  flex: 1.1;
-  transition: box-shadow 0.2s;
-  overflow: hidden;
-  transition: 0.5s;
-  &:hover {
-    box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.1);
-    height: 80%;
-  }
-
-  .top {
-    display: flex;
-    font-size: 10px;
-    margin-bottom: 15px;
-  }
-  .middle {
-    margin-bottom: 8px;
-    font-size: 8px;
+  .text-area {
+    border: none;
+    background-color: transparent;
+    resize: none;
+    width: 100%;
+    min-height: 200px;
   }
 `;
 
@@ -211,101 +252,119 @@ const getStudentRegion = (ID: string) => {
   return regions[Number(ID[2]) - 1];
 };
 
-const getSkills = (skills: string[]) => {
-  return skills.map((skill) => {
-    return {
-      codeName: skill,
-    };
-  });
-};
-
 const getDate = (date: Date) => {
   return date
     ? JSON.stringify(date).split('').slice(1, 11).join('')
     : '????-??-??';
 };
+
+const USER_INFO = 0;
+const USER_PROJECT = 1;
 // TODO any에 대한 타입 수정이 필요합니다.
 export default function MyDetail({ changeEditMode }: any): ReactElement {
+  const [route, setRoute] = useState(USER_INFO);
   const { user } = useAuthState();
 
   return (
-    <Wrapper>
-      <Icons>
-        <Icon iconName="edit" color="black" func={changeEditMode} />
-      </Icons>
-      <Introduction>
-        <Manifesto>
-          <div className="basicInfo">
-            <p>{`${getStudentRegion(user.studentNumber)} ${getStudentClass(
-              user.studentNumber,
-            )}기 ${user.name}`}</p>
-          </div>
-          <div className="track">
-            <Label text="트랙">
-              <p>{user.wishTrack.map((track) => track.codeName).join(', ')}</p>
-            </Label>
-          </div>
-          <div className="position">
-            <StyledLabel text="포지션">
-              <p>{user.wishPositionCode}</p>
-            </StyledLabel>
-          </div>
-          <div className="skills">
-            <Label text="사용 기술">
-              <SkillSelectAutoComplete value={user.skills} disabled={true} />
-            </Label>
-          </div>
-          <div className="introduce">
-            <Label text="자기 소개">
-              <p>{user.introduce}</p>
-            </Label>
-          </div>
-        </Manifesto>
-        <Portrait>
-          <Image
-            src={getImageURL(user.img)}
-            className="default-image"
-            alt="프로필이미지"
-            width={500}
-            height={500}
-          />
-        </Portrait>
-      </Introduction>
-      <div className="name">프로젝트</div>
-      <Projects>
-        {user.projects.length ? (
-          user.projects.map(({ id, name, position, url, introduce }: any) => (
-            <a href={url}>
-              <Project key={id}>
-                <div className="top">
-                  <p>{name}</p>
-                  <p>{position}</p>
-                </div>
-                <div className="introduce">{introduce}</div>
-              </Project>
-            </a>
-          ))
-        ) : (
-          <div>프로젝트가 없습니다.</div>
-        )}
-      </Projects>
-      <div className="name">수상경력</div>
-      <Awards>
-        {user.awards.length ? (
-          user.awards.map(({ id, agency, date, name, introduce }: any) => (
-            <Award key={id}>
-              <div className="top">
-                <p>{agency}</p>
-                <p>{name}</p>
+    <Wrapper isProject={route === USER_PROJECT}>
+      <div className="profile-container">
+        <ProfileImage src={getImageURL(user.img)} size={100} />
+        <Text
+          text={`${getStudentRegion(user.studentNumber)} ${getStudentClass(
+            user.studentNumber,
+          )}기 ${user.name}`}
+          fontSetting="n18m"
+        />
+      </div>
+      <div className="button-container">
+        <button type="button" onClick={() => setRoute(USER_INFO)}>
+          유저 정보
+        </button>
+        <button type="button" onClick={() => setRoute(USER_PROJECT)}>
+          프로젝트
+        </button>
+      </div>
+      <div className="typography">
+        <div className="icons">
+          <Icon iconName="edit" color="black" func={changeEditMode} />
+        </div>
+        {route === USER_INFO ? (
+          <div className="introduction">
+            <div className="portrait">
+              <div className="track">
+                <Label text="트랙" fontSetting="n18b">
+                  <p>
+                    {user.wishTrack.map((track) => track.codeName).join(', ')}
+                  </p>
+                </Label>
               </div>
-              <div className="middle">{getDate(date)}</div>
-              <div>{introduce}</div>
-            </Award>
-          ))
+              <div className="position">
+                <Label text="포지션" fontSetting="n18b">
+                  <p>{user.wishPositionCode}</p>
+                </Label>
+              </div>
+              <div className="skills">
+                <Label text="사용 기술" fontSetting="n18b">
+                  <SkillSelectAutoComplete value={user.skills} disabled />
+                </Label>
+              </div>
+            </div>
+            <div className="manifesto">
+              <div className="introduce">
+                <Label text="자기 소개" fontSetting="n18b">
+                  <Textarea className="text-area" disabled>
+                    {user.introduce}
+                  </Textarea>
+                </Label>
+              </div>
+            </div>
+          </div>
         ) : (
-          <div>수상경력이 없습니다.</div>
+          <>
+            <Label text="프로젝트" fontSetting="n18b">
+              <div className="projects">
+                {user.projects.length ? (
+                  user.projects.map(
+                    ({ id, name, position, url, introduce }: any) => (
+                      <a href={url}>
+                        <div className="project" key={id}>
+                          <div className="top">
+                            <p>{name}</p>
+                            <p>{position}</p>
+                          </div>
+                          <div className="introduce">{introduce}</div>
+                        </div>
+                      </a>
+                    ),
+                  )
+                ) : (
+                  <div>프로젝트가 없습니다.</div>
+                )}
+              </div>
+            </Label>
+            <Label text="수상경력" fontSetting="n18b">
+              <div className="awards">
+                {user.awards.length ? (
+                  user.awards.map(
+                    ({ id, agency, date, name, introduce }: any) => (
+                      <div className="award" key={id}>
+                        <div className="top">
+                          <p>{agency}</p>
+                          <p>{name}</p>
+                        </div>
+                        <div className="middle">{getDate(date)}</div>
+                        <div>{introduce}</div>
+                      </div>
+                    ),
+                  )
+                ) : (
+                  <div>수상경력이 없습니다.</div>
+                )}
+              </div>
+            </Label>
+          </>
         )}
-      </Awards>
+      </div>
     </Wrapper>
   );
 }
