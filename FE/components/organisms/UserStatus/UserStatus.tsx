23752e49960d2183261f7e584ac159ed59eb5f49
dididy <dev.yongjaelee@gmail.com>
2021-08-20 07:44:59 +0900

    fix: 인력풀에서 404뜨면 그 이유를 에러메시지로 출력하도록 한다
    * 이유 설명

--- FE/components/organisms/UserStatus/UserStatus.tsx
+++ FE/components/organisms/UserStatus/UserStatus.tsx
@@ -184,6 +184,7 @@ export default function UserStatus(): ReactElement {
           },
           status,
         }) {
+          console.error(errorMessage);
           setUsers([]);
         }
       })();
